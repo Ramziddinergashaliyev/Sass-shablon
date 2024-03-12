9608d355a82a909a205ec39177013d0d3d9fb07b
RamziddinCoder <mrramziddindeveloper@gmail.com>
2024-03-12 21:01:19 +0500

    first commit

--- js/app.js
+++ js/app.js
@@ -4,3 +4,26 @@ let navBar = document.querySelector(".header__list");
 menu.addEventListener("click",function () {
   navBar.classList.toggle("header__show-list")
 })
+
+
+$(".owl-carousel").owlCarousel({
+  margin: 4,
+  loop: true,
+  autoWidth: true,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 1000,
+  items: 2,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    1000: {
+      items: 1,
+      nav: true,
+      loop: false,
+    },
+  },
+});
